9deaf46c5856c2169a15cbad04e735183550b5da
Kamil Nguyen Van <nvkga1@gmail.com>
2023-08-04 13:07:51 +0200

    feat: update dependencies; clean

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,13 +5,13 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    "prettier",
+    'prettier',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-    "react/prop-types": "off"
+    'react/prop-types': 'off',
   },
 }
